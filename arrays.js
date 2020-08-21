9f2d6c8f89b4af2c9977f3e2df3e40b90cc68f18
Epeli Ratoto <eratoto@yahoo.com>
2020-08-21 20:23:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,6 +33,11 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
 
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
 
 
 
